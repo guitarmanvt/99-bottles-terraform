fc50be14f4319db2f31d073089ee10291f1dd49d
John Anderson <john@andersoninnovative.com>
2019-08-22 11:05:48 -0400

    Format

--- main.tf
+++ main.tf
@@ -6,21 +6,21 @@
 
 locals {
   verse = join("\n", [
-    "",  # blank line between verses
+    "", # blank line between verses
     "%%s bottle%%s of beer on the wall",
     "%%s bottle%%s of beer",
     "%s",
     "%%s bottle%%s of beer on the wall"
   ])
-  lyric = format(local.verse, "take one down, pass it around")
+  lyric  = format(local.verse, "take one down, pass it around")
   ending = format(local.verse, "go to the store, buy some more")
   verses = concat(
-    [for i in range(99,2,-1)  : format(local.lyric, i, "s", i, "s", i - 1, "s")],
+    [for i in range(99, 2, -1) : format(local.lyric, i, "s", i, "s", i - 1, "s")],
     [for i in range(1, 0, -1) : format(local.lyric, i, "", i, "", "no more", "s")],
-    [for i in range(0, -1, -1): format(local.ending, "no more", "s", "no more", "s", 99, "s")]
+    [for i in range(0, -1, -1) : format(local.ending, "no more", "s", "no more", "s", 99, "s")]
   )
 }
 
 output "ninety-nine-bottles-of-beer" {
-    value = join("\n", local.verses)
+  value = join("\n", local.verses)
 }
